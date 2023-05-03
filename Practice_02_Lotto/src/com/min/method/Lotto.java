877eba65c360fad5288046fd778261fa53396f3f
s5hy25ni <s5hy25ni@gmail.com>
2023-05-04 00:34:38 +0900

    lotto class

--- Practice_02_Lotto/src/com/min/method/Lotto.java
+++ Practice_02_Lotto/src/com/min/method/Lotto.java
@@ -3,23 +3,30 @@ package com.min.method;
 import java.util.Arrays;
 
 public class Lotto {
-	private int[] nums;
+	private int[] selNums;
 	private boolean manual = false;
 
 	public Lotto(int size) { // size가 6이면 자동, size가 7이면 winLotto
-		nums = new int[size];
-		inputNums(nums);
+		selNums = new int[size];
+		for (int i = 0; i<size; i++) {
+			if()
+		}
 	}
 
-	public Lotto(int[] userNums) { // arguments가 int[]이면 수동
-		nums = new int[userNums.length];
-		for (int i = 0; i < nums.length; i++) {
-			nums[i] = userNums[i];
+	public Lotto(int[] userSelNums) { // arguments가 int[]이면 수동
+		selNums = new int[userSelNums.length];
+		for (int i = 0; i < selNums.length; i++) {
+			selNums[i] = userSelNums[i];
 		}
 		manual = true;
 	}
+	
+	public Lotto(int[] userSelNums, int autoStart) {
+		
+	}
 
 	private void autoLotto(int[] newNums) {
+		int idx = 1;
 		while (true) {
 			int num = randomNum();
 			if (!checkDup(idx, num)) {
@@ -34,6 +41,17 @@ public class Lotto {
 	private int randomNum() {
 		int num = (int) (Math.random() * 45) + 1;
 		return num;
+	}
+	
+	boolean checkDup(int[] nums, int chkNum) {
+		boolean isc = false;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == chkNum) {
+				isc = true;
+				break;
+			}
+		}
+		return isc;
 	}
 	
 	@Override
