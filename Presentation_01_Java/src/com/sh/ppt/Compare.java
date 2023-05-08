3334a2bced4113b485f903e93a2d20540ffeed13
s5hy25ni <s5hy25ni@gmail.com>
2023-05-08 14:12:15 +0900

    PPT 발표 준비 - String method

--- Presentation_01_Java/src/com/sh/ppt/Compare.java
+++ Presentation_01_Java/src/com/sh/ppt/Compare.java
@@ -4,56 +4,67 @@ public class Compare {
 	
 	public static void main(String[] args) {
 
-		System.out.println();
-		String str1 = "JavaString";
-		String str2 = "JavaString";
-		String str3 = str1;
+//		System.out.println();
+//		String str1 = "JavaString";
+//		String str2 = "JavaString";
+//		String str3 = str1;
 		String str4 = new String("JavaString");
-		System.out.println("str1==str2 ? "+(str1==str2));
-		System.out.println("str1==str3 ? "+(str1==str3));
-		System.out.println("str1==str4 ? "+(str1==str4));
+//		System.out.println("str1==str2 ? "+(str1==str2));
+//		System.out.println("str1==str3 ? "+(str1==str3));
+//		System.out.println("str1==str4 ? "+(str1==str4));
+//		
+//		System.out.println();
+//		String str5 = "Java"+"String";
+//		String temp1 = "Java";
+//		String temp2 = "String";
+//		String str6 = temp1+temp2;
+//		System.out.println("str1==str5 ? "+(str1==str5));
+//		System.out.println("str1==str6 ? "+(str1==str6));
+//		
+//		System.out.println();
+//		System.out.println("str1.equals(str2) = "+str1.equals(str2));
+//		System.out.println("str1.equals(str3) = "+str1.equals(str3));
+//		System.out.println("str1.equals(str4) = "+str1.equals(str4));
+//		System.out.println("str1.equals(str5) = "+str1.equals(str5));
+//		System.out.println("str1.equals(str6) = "+str1.equals(str6));
+//		
+//		System.out.println();
+//		System.out.println("str1 hashCode = "+str1.hashCode());
+//		System.out.println("str2 hashCode = "+str2.hashCode());
+//		System.out.println("str3 hashCode = "+str3.hashCode());
+//		System.out.println("str4 hashCode = "+str4.hashCode());
+//		System.out.println("str5 hashCode = "+str5.hashCode());
+//		System.out.println("str6 hashCode = "+str6.hashCode());
+//		
+//		System.out.println();
+//		System.out.println("str1 identityHashCode = "+System.identityHashCode(str1));
+//		System.out.println("str2 identityHashCode = "+System.identityHashCode(str2));
+//		System.out.println("str3 identityHashCode = "+System.identityHashCode(str3));
+//		System.out.println("str4 identityHashCode = "+System.identityHashCode(str4));
+//		System.out.println("str5 identityHashCode = "+System.identityHashCode(str5));            
+//		System.out.println("str6 identityHashCode = "+System.identityHashCode(str6));            
+//		
+//		System.out.println();
+//		System.out.println("str1.intern() = "+str1.intern());
+//		System.out.println("str2.intern() = "+str2.intern());
+//		System.out.println("str3.intern() = "+str3.intern());
+//		System.out.println("str4.intern() = "+str4.intern());
+//		System.out.println("str5.intern() = "+str5.intern());
+//		System.out.println("str6.intern() = "+str6.intern());
+//		
+//		System.out.println();
+//		System.out.println("str1.intern()==str4.intern() ? "+(str1.intern()==str4.intern()));
+//		System.out.println("str1.intern()==str6.intern() ? "+(str1.intern()==str6.intern()));
 		
-		System.out.println();
-		String str5 = "Java"+"String";
-		String temp1 = "Java";
-		String temp2 = "String";
-		String str6 = temp1+temp2;
-		System.out.println("str1==str5 ? "+(str1==str5));
-		System.out.println("str1==str6 ? "+(str1==str6));
 		
-		System.out.println();
-		System.out.println("str1.equals(str2) = "+str1.equals(str2));
-		System.out.println("str1.equals(str3) = "+str1.equals(str3));
-		System.out.println("str1.equals(str4) = "+str1.equals(str4));
-		System.out.println("str1.equals(str5) = "+str1.equals(str5));
-		System.out.println("str1.equals(str6) = "+str1.equals(str6));
+		String str1 = "Java";
+		String str2 = new String("Java");
 		
-		System.out.println();
-		System.out.println("str1 hashCode = "+str1.hashCode());
-		System.out.println("str2 hashCode = "+str2.hashCode());
-		System.out.println("str3 hashCode = "+str3.hashCode());
-		System.out.println("str4 hashCode = "+str4.hashCode());
-		System.out.println("str5 hashCode = "+str5.hashCode());
-		System.out.println("str6 hashCode = "+str6.hashCode());
-		
-		System.out.println();
-		System.out.println("str1 identityHashCode = "+System.identityHashCode(str1));
-		System.out.println("str2 identityHashCode = "+System.identityHashCode(str2));
-		System.out.println("str3 identityHashCode = "+System.identityHashCode(str3));
-		System.out.println("str4 identityHashCode = "+System.identityHashCode(str4));
-		System.out.println("str5 identityHashCode = "+System.identityHashCode(str5));            
-		System.out.println("str6 identityHashCode = "+System.identityHashCode(str6));            
-		
-		System.out.println();
-		System.out.println("str1.intern() = "+str1.intern());
-		System.out.println("str2.intern() = "+str2.intern());
-		System.out.println("str3.intern() = "+str3.intern());
-		System.out.println("str4.intern() = "+str4.intern());
-		System.out.println("str5.intern() = "+str5.intern());
-		System.out.println("str6.intern() = "+str6.intern());
-		
-		System.out.println();
-		System.out.println("str1.intern()==str4.intern() ? "+(str1.intern()==str4.intern()));
-		System.out.println("str1.intern()==str6.intern() ? "+(str1.intern()==str6.intern()));
+		System.out.println(str1.hashCode());
+		System.out.println(str2.hashCode());
+		System.out.println(System.identityHashCode(str1));
+		System.out.println(System.identityHashCode(str2));
+		System.out.println(str1.intern());
+		System.out.println(str2.intern());
 	}
 }
---
+++ Presentation_01_Java/src/com/sh/ppt/Method.java
@@ -0,0 +1,87 @@
+package com.sh.ppt;
+
+import java.util.Arrays;
+
+public class Method {
+	public static void main(String[] args) {
+//		String str = String.format("%d, %s, %c", 1, "Java", 'a');
+//		System.out.println(str);
+		
+		//			   543210
+//		String str2 = "123123";
+//		System.out.println(str2.indexOf("1"));
+//		System.out.println(str2.indexOf("1", 1));
+//		System.out.println(str2.indexOf("1", 4));
+//		System.out.println(str2.indexOf("4"));
+		
+//		System.out.println(str2.lastIndexOf("3"));
+//		System.out.println(str2.lastIndexOf("3", 0));
+//		System.out.println("fromindex = 0 : "+str2.lastIndexOf("3", 0));
+//		System.out.println("fromindex = 0 : "+str2.lastIndexOf("2", 0));
+//		System.out.println("fromindex = 0 : "+str2.lastIndexOf("1", 0));
+//		System.out.println("------------------------------------------");
+//		System.out.println("fromindex = 1 : "+str2.lastIndexOf("3", 1));
+//		System.out.println("fromindex = 1 : "+str2.lastIndexOf("2", 1));
+//		System.out.println("fromindex = 1 : "+str2.lastIndexOf("1", 1));
+//		System.out.println("------------------------------------------");
+//		System.out.println("fromindex = 2 : "+str2.lastIndexOf("3", 2));
+//		System.out.println("fromindex = 2 : "+str2.lastIndexOf("2", 2));
+//		System.out.println("fromindex = 2 : "+str2.lastIndexOf("1", 2));
+//		System.out.println("------------------------------------------");
+//		System.out.println("fromindex = 3 : "+str2.lastIndexOf("3", 3));
+//		System.out.println("fromindex = 3 : "+str2.lastIndexOf("2", 3));
+//		System.out.println("fromindex = 3 : "+str2.lastIndexOf("1", 3));
+//		System.out.println("------------------------------------------");
+//		System.out.println("fromindex = 4 : "+str2.lastIndexOf("3", 4));
+//		System.out.println("fromindex = 4 : "+str2.lastIndexOf("2", 4));
+//		System.out.println("fromindex = 4 : "+str2.lastIndexOf("1", 4));
+//		System.out.println("------------------------------------------");
+//		System.out.println("fromindex = 5 : "+str2.lastIndexOf("3", 5));
+//		System.out.println("fromindex = 5 : "+str2.lastIndexOf("2", 5));
+//		System.out.println("fromindex = 5 : "+str2.lastIndexOf("1", 5));
+//		System.out.println(str2.lastIndexOf("3", 2));
+//		System.out.println(str2.lastIndexOf("3", 3));
+//		System.out.println(str2.lastIndexOf("4"));
+		
+//		String str3 = "청사과 풋사과";
+//		System.out.println(str3.matches("(.*)사과(.*)"));
+//		System.out.println(str3.matches(".*\\b사과\\b.*"));
+//		
+//		String str4 = "사과 청사과 풋사과";
+//		System.out.println(str4.matches(".*\\b사과\\b.*"));
+		
+//		String str = "abcd1234efgh";
+//		System.out.println(str.replace("abcd", "ijkl"));
+//		System.out.println(str.replaceAll("[a-z]{4}","ijkl"));
+		
+//		String str = "12345";
+//		char[] charAt_arr = new char[str.length()];
+//		for(int i =0; i < str.length(); i++) {
+//			charAt_arr[i] = str.charAt(i);
+//		}
+//		char[] toCharArray_arr = str.toCharArray();
+//
+//		System.out.println(Arrays.toString(charAt_arr));
+//		System.out.println(Arrays.toString(toCharArray_arr));
+//		
+//		String address1 = "서울시 금천구 어쩌구";
+//	    String address2 = "대전시 유성구 어쩌구";
+//	    System.out.println(address1.startsWith("서울시"));
+//	    System.out.println(address2.startsWith("서울시"));
+//	    
+//	    String email1 = "exam01@gmail.com";
+//	    String email2 = "exam02@naver.com";
+//	    System.out.println(email1.endsWith("gmail.com"));
+//	    System.out.println(email2.endsWith("gmail.com"));
+
+		Search search = new Search();
+		
+		String text = "endendendend";
+		String target = "end";
+		search.nextSearch(text,target);
+		
+		
+		
+		
+	}
+}
