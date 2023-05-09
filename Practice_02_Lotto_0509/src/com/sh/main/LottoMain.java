bf10011f3d902fa715b73d79836c9807547216ca
s5hy25ni <s5hy25ni@gmail.com>
2023-05-09 20:50:43 +0900

    로또 구조 어쩌냐,,,,

---
+++ Practice_02_Lotto_0509/src/com/sh/main/LottoMain.java
@@ -0,0 +1,15 @@
+package com.sh.main;
+
+import java.time.Period;
+import java.util.Calendar;
+
+import com.sh.method.Paper;
+import com.sh.method.Process;
+
+public class LottoMain {
+
+	public static void main(String[] args) {
+	}
+	
+	
+}
