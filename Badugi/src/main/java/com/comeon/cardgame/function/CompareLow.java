be73c988d38a586bdeafea6b3bb68cea40f05210
s5hy25ni <s5hy25ni@gmail.com>
2023-05-23 06:24:34 +0900

    2023052_족보 어느정도 완성

---
+++ Badugi/src/main/java/com/comeon/cardgame/function/CompareLow.java
@@ -0,0 +1,14 @@
+package com.comeon.cardgame.function;
+
+import java.util.List;
+
+import com.comeon.cardgame.tool.Player;
+
+public abstract class CompareLow extends CompareImpl{
+
+	public CompareLow(List<Player> players) {
+		super(players);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Badugi/src/main/java/com/comeon/cardgame/tool/CardCase.java
@@ -0,0 +1,34 @@
+package com.comeon.cardgame.tool;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardCase {
+
+	private List<CardOne> cardcase;
+	
+	public CardCase() {
+		cardcase = new ArrayList<CardOne>();
+		make();
+	}
+	
+	private void make() {
+		int cnt = 0;
+		while(true) {
+			CardOne card = new CardOne();
+			if(!cardcase.contains(card)) {
+				cardcase.add(card);
+				cnt++;
+			}
+
+			if(cnt==CardOne.shape.length*CardOne.number.length) {
+				break;
+			}
+		}
+	}
+
+	public List<CardOne> getCardcase() {
+		return cardcase;
+	}
+
+}
