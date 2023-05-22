94fcea2e3204b0519fe04232ff288c320bdf48c8
s5hy25ni <s5hy25ni@gmail.com>
2023-05-22 18:41:09 +0900

    20230522

---
+++ Practice_03_CardGame_SVN/src/main/java/com/comeon/cardgame/tool/CardOne.java
@@ -0,0 +1,50 @@
+package com.comeon.cardgame.tool;
+
+public class CardOne {
+
+	private String one;
+	private int size;
+
+	public static enum Shape {
+
+		CLOVER("♣"), HEART("♥"), DIAMOND("◆"), SPACE("♠");
+		
+		public String value;
+		private Shape(String s) {
+			value  = s;
+		}
+		public String getValue() {
+			return value;
+		}
+	}
+	
+	public enum Number {
+		
+		A("A"), TWO("2"), THREE("3"), FOUR("4"), FIVE("5"), SIX("6"), SEVEN("7"), EIGHT("8"), NINE("9"), TEN("10"), J("J"), Q("Q"), K("K");
+		
+		public String value;
+		private Number(String n) {
+			value  = n;
+		}
+	}
+
+	CardOne() {
+		Shape[] shape = Shape.values();
+		Number[] number = Number.values();
+		int s = (int)(Math.random()*shape.length);
+		int n = (int)(Math.random()*number.length);
+		
+		one = shape[s].value+number[n].value;
+		size = shape[s].ordinal()+(number[n].ordinal()+1)*10;
+	}
+
+	String getOne() {
+		return one;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	
+}
